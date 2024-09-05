d3acc1ec3ac64a2c4ea29394ebae38e20bd6a52b
Kelvin <kelvin.ojeda@epn.edu.ec>
2024-09-05 10:45:55 -0500

    ADD: Tailwind styling to the project and installed testing dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import EntryList from "./components/EntryList";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        // <div className="App">
+        // <EntryList/>
+        // </div>
+    )
 }
 
 export default App;
