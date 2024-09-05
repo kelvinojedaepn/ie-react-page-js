ae2636befc2476c721c19bdde97605b44dc6948b
Kelvin <kelvin.ojeda@epn.edu.ec>
2024-09-05 11:05:12 -0500

    UPDATE: Modify the util.js to count words

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,5 +1,9 @@
 const countWords = (text) => {
-
+    return text
+        .replace(/[^\w\s]/g, '') // Remove non-alphanumeric and non-space characters
+        .split(/\s+/) // Split the text into an array by one or more spaces
+        .filter(Boolean) // Remove empty elements from the array
+        .length; // Count the number of elements in the array
 };
 
 export default countWords;
